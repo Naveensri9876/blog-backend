016eec1ee97e8e13e855e961a365424926cbf928
Navensri9876 <naveensri.elsa@gmail.com>
2025-06-20 13:55:44 +0530

    Backup local changes before pulling

--- models/Post.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const postSchema = new mongoose.Schema({
-  title: String,
-  content: String,
-  authorId: String,
-}, { timestamps: true });
-
-export default mongoose.model('Post', postSchema);
